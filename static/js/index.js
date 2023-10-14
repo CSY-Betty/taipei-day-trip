f2b6f3d63609cc8d7b35034e4bfea0828dbf8a59
Betty <qwe50929@gmail.com>
2023-10-15 01:04:20 +0800

    refact index

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,63 @@
-import * as divBuilder from './div-builder.js';
+import { renderAttractions, renderMrtList } from './div-builder.js';
 import { fetchData } from './fetchAPI.js';
 
 let nextPage = null;
 let isLoading = false;
 let searchQuery = '';
 
-// 點擊list
-function mrtButtonHandler() {
+function fetchAttractionData(searchQuery = null, nextPage = null) {
+	let url = '/api/attractions';
+	if (searchQuery !== null) {
+		url += `?keyword=${searchQuery}`;
+	}
+
+	if (nextPage !== null) {
+		url += searchQuery ? `&page=${nextPage}` : `?page=${nextPage}`;
+	}
+
+	return fetchData(
+		url,
+		'GET',
+		{
+			'Content-Type': 'application/json',
+		},
+		null
+	);
+}
+
+function fetchMrtData() {
+	return fetchData(
+		'/api/mrts',
+		'GET',
+		{
+			'Content-Type': 'application/json',
+		},
+		null
+	);
+}
+
+async function searchByKeyword(searchQuery) {
+	const attractionData = await fetchAttractionData(searchQuery);
+
+	nextPage = attractionData.nextPage;
+	renderAttractions(attractionData);
+}
+
+// 關鍵字查詢
+function handleSearchButton() {
+	document
+		.getElementById('searchButton')
+		.addEventListener('click', function () {
+			searchQuery = document.getElementById('searchBox').value;
+			clearCurrentContent();
+			searchByKeyword(searchQuery);
+		});
+}
+
+handleSearchButton();
+
+// list_bar搜尋
+function handleMrtButton() {
 	let mrtButtons = document.querySelectorAll('.mrt_button');
 	mrtButtons.forEach((button) => {
 		button.addEventListener('click', function () {
@@ -16,28 +67,55 @@ function mrtButtonHandler() {
 			clearCurrentContent();
 			searchByKeyword(searchQuery);
 
-			// 更換搜尋欄顯示文字
 			let searchBox = document.getElementById('searchBox');
-			searchBox.value = '';
-			searchBox.setAttribute('placeholder', buttonText);
+			searchBox.value = buttonText;
 		});
 	});
 }
 
-function loadAttractionData() {
-	fetchData(
-		'/api/attractions',
-		'GET',
-		{
-			'Content-Type': 'application/json',
-		},
-		null
-	).then((attractionData) => {
-		nextPage = attractionData.nextPage;
-		searchQuery = null;
-		divBuilder.renderAttractions(attractionData);
-	});
+// list_bar左右捲動
+function mrtScrollList(direction) {
+	const mrtsContainer = document.getElementById('mrts_container');
+	const currentScrollLeft = mrtsContainer.scrollLeft;
+	const scrollAmount = mrtsContainer.clientWidth - 20;
+	let newScrollLeft;
+
+	if (direction === 'left') {
+		newScrollLeft = currentScrollLeft - scrollAmount;
+	} else if (direction === 'right') {
+		newScrollLeft = currentScrollLeft + scrollAmount;
+	}
+	mrtsContainer.scroll({ top: 0, left: newScrollLeft, behavior: 'smooth' });
 }
+
+function setupListBarScroll() {
+	let leftBtn = document.getElementById('left_btn');
+	let rightBtn = document.getElementById('right_btn');
+
+	leftBtn.addEventListener('click', () => mrtScrollList('left'));
+	rightBtn.addEventListener('click', () => mrtScrollList('right'));
+}
+
+function initListBar() {
+	fetchMrtData().then((MrtData) => {
+		renderMrtList(MrtData);
+		handleMrtButton();
+	});
+	setupListBarScroll();
+}
+
+initListBar();
+
+// 景點載入
+async function loadAttractionData() {
+	const attractionData = await fetchAttractionData();
+
+	nextPage = attractionData.nextPage;
+	searchQuery = null;
+	renderAttractions(attractionData);
+}
+
+loadAttractionData();
 
 // 清空現有頁面
 function clearCurrentContent() {
@@ -53,35 +131,8 @@ function clearCurrentContent() {
 	});
 }
 
-// 景點查詢按鈕
-const searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', function () {
-	searchQuery = document.getElementById('searchBox').value;
-	clearCurrentContent();
-	searchByKeyword(searchQuery);
-});
-
-// 關鍵字查詢
-function searchByKeyword(searchQuery) {
-	fetchData(`/api/attractions?keyword=${searchQuery}`, 'GET', {
-		'Content-Type': 'application/json',
-	}).then((data) => {
-		const attractionData = data.data;
-		if (attractionData.length === 0) {
-			divBuilder.renderAttractions(data);
-		} else {
-			nextPage = data.nextPage;
-			divBuilder.renderAttractions(data);
-		}
-	});
-}
-
 // 滾動加載監聽
-window.addEventListener('scroll', function (e) {
-	loadMore(searchQuery);
-});
-
-function loadMore(searchQuery) {
+async function handleScrollAndLoadMore(searchQuery) {
 	if (nextPage === null || isLoading) {
 		return; // 停止加載
 	}
@@ -90,84 +141,27 @@ function loadMore(searchQuery) {
 	const scrollHeight = document.documentElement.scrollHeight;
 	const scrollTop = window.scrollY;
 	const clientHeight = window.innerHeight;
-	const scrollThreshold = 100; // 設置滾動閾值，用於觸發加載
+	const scrollThreshold = 100;
 
 	// 當滾動到接近頁面底部時，觸發加載
 	if (scrollHeight - scrollTop - clientHeight < scrollThreshold) {
 		isLoading = true;
+		const attractionData = await fetchAttractionData(searchQuery, nextPage);
+		renderAttractions(attractionData);
 
-		const searchurl =
-			searchQuery !== null
-				? `/api/attractions?keyword=${searchQuery}&page=${nextPage}`
-				: `/api/attractions?page=${nextPage}`;
+		nextPage = attractionData.nextPage;
 
-		fetchData(
-			searchurl,
-			'GET',
-			{
-				'Content-Type': 'application/json',
-			},
-			null
-		).then(function (responseData) {
-			divBuilder.renderAttractions(responseData);
+		if (nextPage === null) {
+			window.removeEventListener('scroll', handleScrollAndLoadMore);
+		}
 
-			nextPage = responseData.nextPage;
-
-			if (nextPage === null) {
-				window.removeEventListener('scroll', loadMore);
-			}
-
-			isLoading = false;
-		});
+		isLoading = false;
 	}
 }
 
-// 清空搜尋框
-function resetSearchBox() {
-	const searchBox = document.getElementById('searchBox');
-	searchBox.value = '';
-	searchBox.setAttribute('placeholder', '輸入景點名稱查詢');
-}
-
-// listBar左右滾動
-function listBarScroll() {
-	let leftBtn = document.getElementById('left_btn');
-	let rightBtn = document.getElementById('right_btn');
-
-	leftBtn.addEventListener('click', () => scroll('left'));
-	rightBtn.addEventListener('click', () => scroll('right'));
-}
-
-function scroll(direction) {
-	const mrtsContainer = document.getElementById('mrts_container');
-	const currentScrollLeft = mrtsContainer.scrollLeft;
-	const scrollAmount = mrtsContainer.clientWidth - 20;
-	let newScrollLeft;
-
-	if (direction === 'left') {
-		newScrollLeft = currentScrollLeft - scrollAmount;
-	} else if (direction === 'right') {
-		newScrollLeft = currentScrollLeft + scrollAmount;
-	}
-	mrtsContainer.scroll({ top: 0, left: newScrollLeft, behavior: 'smooth' });
-}
-
-loadAttractionData();
-
-fetchData(
-	'/api/mrts',
-	'GET',
-	{
-		'Content-Type': 'application/json',
-	},
-	null
-).then((MrtData) => {
-	divBuilder.renderMrtList(MrtData);
-	mrtButtonHandler();
+window.addEventListener('scroll', function () {
+	handleScrollAndLoadMore(searchQuery);
 });
-
-// list_bar左右捲動
-listBarScroll();
 
 function handleAttractionClick() {
 	let attraction = document.getElementById('attractions');
