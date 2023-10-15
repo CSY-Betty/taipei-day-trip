7a9af0a5e60b34d207b7cc4ea2f66f2d01997b53
Betty <qwe50929@gmail.com>
2023-10-15 19:51:45 +0800

    refact attractionPage

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,137 +1,128 @@
+import { fetchData, fetchUserData } from './fetchAPI.js';
 // 返回初始畫面
-let titleButton = document.getElementById("title");
-titleButton.addEventListener("click", function(){
-	window.location.href = `/`
+let titleButton = document.getElementById('title');
+titleButton.addEventListener('click', function () {
+	window.location.href = `/`;
 });
-
 
 // 取得景點id
 const currentUrl = window.location.pathname;
 const attractionId = currentUrl.match(/\/attraction\/(\d+)/)[1];
 
-
 function getAttractionData() {
-	return fetch(`/api/attraction/${attractionId}`, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-	.then(response => response.json());
-};
+	return fetchData(`/api/attraction/${attractionId}`, 'GET', {
+		'Content-Type': 'application/json',
+	});
+}
 
-
-function createAttractionInfo(data) {
-	
-	const attractionData = data.data;
-	const attractionName = attractionData.name;
-	const attractionCategory = attractionData.category;
-	const attractionMrt = attractionData.mrt;
-	const attractionDescript = attractionData.description;
-	const attractionAddress= attractionData.address;
-	const attractionTrans = attractionData.transport;
-	const attractionImages = attractionData.images;
-
-	// 景點圖片區塊
-	const imageSlide = document.getElementById("imageSlide");
+function createImageGallery(data) {
+	const images = data.data.images;
+	const imageSlide = document.getElementById('imageSlide');
+	const slideDot = document.getElementById('slideDot');
 	const firstNode = imageSlide.firstChild;
 
-	// 製作所有圖片
-	attractionImages.forEach(imageUrl => {
-		const image = document.createElement("img");
-		image.classList.add("slide__item", "fades")
+	images.forEach((imageUrl) => {
+		const image = document.createElement('img');
+		image.className = 'slide__item fades';
 		image.src = imageUrl;
 		imageSlide.insertBefore(image, firstNode);
 	});
 
-	const slideDot = document.getElementById("slideDot");
-	const dotNum = attractionImages.length
-
-	
-	for (let i = 0; i < dotNum; i++) {
-		const dot = document.createElement("div");
-		dot.classList.add("dot");
+	for (let i = 0; i < images.length; i++) {
+		const dot = document.createElement('div');
+		dot.className = 'dot';
 		slideDot.appendChild(dot);
 	}
+}
+
+function createAttractionInfo(data) {
+	const attractionData = data.data;
+	const { name, category, mrt, description, address, transport } =
+		attractionData;
 
 	// 景點標題區塊
-	const menu = document.getElementById("menu");
+	const menu = document.getElementById('menu');
 
-	const title = document.createElement("span");
-	title.textContent = attractionName;
-	title.classList.add("menu__title", "h3-bold");
+	const title = document.createElement('span');
+	title.textContent = name;
+	title.classList.add('menu__title', 'h3-bold');
 
 	menu.appendChild(title);
 
 	// 將description顯示在最前面
 	menu.insertBefore(title, menu.firstChild);
 
-	const subTitle = document.createElement("span");
-	subTitle.classList.add("menu__subtitle", "body-med");
+	const subTitle = document.createElement('span');
+	subTitle.classList.add('menu__subtitle', 'body-med');
 
-	const tagNode = document.createTextNode(`${attractionCategory} at ${attractionMrt}`);
+	const tagNode = document.createTextNode(`${category} at ${mrt}`);
 
 	subTitle.appendChild(tagNode);
 	menu.insertBefore(subTitle, title.nextSibling);
-	
 
 	// 景點描述區塊
-	const description = document.getElementById("description");
+	const descriptionContainer = document.getElementById('description');
 
-	const introduce = document.createElement("span");
-	introduce.textContent = attractionDescript;
-	introduce.classList.add("description__introduce", "content-reg")
+	const introduce = document.createElement('span');
+	introduce.textContent = description;
+	introduce.classList.add('description__introduce', 'content-reg');
 
-	description.insertBefore(introduce, description.firstChild);
+	descriptionContainer.insertBefore(
+		introduce,
+		descriptionContainer.firstChild
+	);
 
+	const addressContainer = document.createElement('span');
+	addressContainer.textContent = address;
+	addressContainer.classList.add('description__address', 'content-reg');
 
-	const address = document.createElement("span");
-	address.textContent = attractionAddress;
-	address.classList.add("description__address", "content-reg")
+	descriptionContainer.insertBefore(
+		addressContainer,
+		descriptionContainer.childNodes[3]
+	);
 
-	description.insertBefore(address, description.childNodes[3]);
+	const transportContainer = document.createElement('span');
+	transportContainer.textContent = transport;
+	transportContainer.classList.add('description__transport', 'content-reg');
 
-	const transport = document.createElement("span");
-	transport.textContent = attractionTrans;
-	transport.classList.add("description__transport", "content-reg")
-
-	description.insertBefore(transport, description.childNodes[6]);
+	descriptionContainer.insertBefore(
+		transportContainer,
+		descriptionContainer.childNodes[6]
+	);
 }
 
-
-
 // 預定按鈕
-const timeMorning = document.getElementById("timeMorning");
-const timeAfternoon = document.getElementById("timeAfternoon");
-timeAfternoon.addEventListener("click", function() {
-	priceFee = document.getElementById("priceFee")
+const timeMorning = document.getElementById('timeMorning');
+const timeAfternoon = document.getElementById('timeAfternoon');
+timeAfternoon.addEventListener('click', function () {
+	priceFee = document.getElementById('priceFee');
 	priceFee.textContent = '新台幣 2500元';
-})
+});
 
-timeMorning.addEventListener("click", function() {
-	priceFee = document.getElementById("priceFee")
+timeMorning.addEventListener('click', function () {
+	priceFee = document.getElementById('priceFee');
 	priceFee.textContent = '新台幣 2000元';
-})
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-    getAttractionData().then(attractionData => {
-        createAttractionInfo(attractionData);
+document.addEventListener('DOMContentLoaded', function () {
+	getAttractionData().then((attractionData) => {
+		createAttractionInfo(attractionData);
+		createImageGallery(attractionData);
 
 		let slideIndex = 1;
 
 		showSlide(slideIndex);
 
-		let prev = document.getElementById("prev");
-		prev.addEventListener("click", divideSlides, false);
+		let prev = document.getElementById('prev');
+		prev.addEventListener('click', divideSlides, false);
 
-		let next = document.getElementById("next");
-		next.addEventListener("click", plusSlides, false);
+		let next = document.getElementById('next');
+		next.addEventListener('click', plusSlides, false);
 
-		const selectdot = document.querySelectorAll(".dot");
-		for (let i = 0; i <selectdot.length; i++) {
-			selectdot[i].addEventListener("click", function(e) {
+		const selectdot = document.querySelectorAll('.dot');
+		for (let i = 0; i < selectdot.length; i++) {
+			selectdot[i].addEventListener('click', function (e) {
 				showSlide((slideIndex = i + 1));
-
 			});
 		}
 
@@ -139,81 +130,82 @@ document.addEventListener("DOMContentLoaded", function() {
 			showSlide((slideIndex += 1));
 		}
 
-
 		function divideSlides() {
 			showSlide((slideIndex -= 1));
 		}
 
 		function showSlide(num) {
-			let slides = document.getElementsByClassName("slide__item");
-			let dots = document.getElementsByClassName("dot");
-			
+			let slides = document.getElementsByClassName('slide__item');
+			let dots = document.getElementsByClassName('dot');
+
 			if (num > slides.length) {
 				slideIndex = 1;
 			}
 
 			if (num < 1) {
-				slideIndex =slides.length;
+				slideIndex = slides.length;
 			}
 
 			for (let i = 0; i < slides.length; i++) {
-				slides[i].style.display = "none";
+				slides[i].style.display = 'none';
 			}
 
 			for (let i = 0; i < dots.length; i++) {
-				dots[i].className = dots[i].className.replace("active", "")
+				dots[i].className = dots[i].className.replace('active', '');
 			}
-			slides[slideIndex - 1].style.display = "block";
-			dots[slideIndex - 1].className += " active";
+			slides[slideIndex - 1].style.display = 'block';
+			dots[slideIndex - 1].className += ' active';
 		}
 	});
 });
 
-
-
-
-let orderCheck = document.getElementById("orderCheck")
-let loginDialog = document.getElementById("loginDialog")
-orderCheck.addEventListener("click", function() {
-	const token = localStorage.getItem("token");
-	let dateSelect = document.getElementById("dateSelect")
+let orderCheck = document.getElementById('orderCheck');
+let loginDialog = document.getElementById('loginDialog');
+orderCheck.addEventListener('click', function () {
+	const token = localStorage.getItem('token');
+	let dateSelect = document.getElementById('dateSelect');
 	let selectedTime;
-	let priceText = priceFee.textContent
+	let priceText = priceFee.textContent;
 	let bookingPrice = parseInt(priceText.replace(/\D/g, ''), 10);
 	if (timeMorning.checked) {
-		selectedTime = "morning"
+		selectedTime = 'morning';
+	} else if (timeAfternoon.checked) {
+		selectedTime = 'afternoon';
 	}
-	else if (timeAfternoon.checked) {
-		selectedTime = "afternoon"
-	}
+	let imageSlide = document.getElementById('imageSlide');
+	let firstImage = imageSlide.querySelector('img').getAttribute('src');
 
-	firstImage = imageSlide.querySelector('img').getAttribute('src');
+	let attractionName = menu.querySelector('.menu__title').textContent;
+	let attractionAddress = description.querySelector(
+		'.description__address'
+	).textContent;
 
-	attractionName = menu.querySelector('.menu__title').textContent;
-	attractionAddress = description.querySelector('.description__address').textContent;
-
-	bookingData = {
+	let bookingData = {
 		attractionId: attractionId,
 		attractionName: attractionName,
 		attractionAddress: attractionAddress,
 		date: dateSelect.value,
 		time: selectedTime,
 		price: bookingPrice,
-		attractionImage: firstImage
-	}
+		attractionImage: firstImage,
+	};
 	if (token) {
-		fetch(`/api/booking`, {method: "POST", headers:{"Content-Type": "application/json", "Authorization": `Bearer ${token}`}, body: JSON.stringify(bookingData)})
-		.then(response => {
-			if (response.status === 200) {
-				window.location.href='/booking';
-			}
-		})
-		.catch(error => {
-			console.log("錯誤: ",error);
-		})
-	}
-	else {
+		fetchUserData(
+			'/api/booking',
+			'POST',
+			{
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			JSON.stringify(bookingData)
+		)
+			.then(() => {
+				window.location.href = '/booking';
+			})
+			.catch((error) => {
+				console.log('錯誤: ', error);
+			});
+	} else {
 		loginDialog.showModal();
 	}
-})
-
+});
