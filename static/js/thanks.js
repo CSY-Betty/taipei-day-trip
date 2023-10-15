9605f299d02b724d5000fe1633a35dd10b29054c
Betty <qwe50929@gmail.com>
2023-10-15 23:45:51 +0800

    feat: fetchdata

--- static/js/thanks.js
+++ static/js/thanks.js
@@ -1,27 +1,34 @@
+import { fetchData } from './fetchAPI.js';
+
 let url = window.location.href;
 let urlNumber = url.match(/number=(\d+)/)[1];
-const token = localStorage.getItem("token");
+let token = localStorage.getItem('token');
 
 function getOrderNumber() {
-	return fetch(`/api/order/${urlNumber}`, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json","Authorization": `Bearer ${token}`
-		},
-	})
-	.then(response => response.json());
-};
+	return fetchData(`/api/order/${urlNumber}`, 'GET', {
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${token}`,
+	});
+}
 
 function renderOrderNumber(data) {
-    let orderNumber = document.getElementById('orderNumber')
-    orderNumber.textContent = data
+	let orderNumber = document.getElementById('orderNumber');
+	orderNumber.textContent = data;
 }
 
-function cleanBooking(data){
-    fetch(`/api/booking`, {method: 'DELETE', headers: {"Content-Type": "application/json", "Authorization": `Bearer ${token}`}, body: JSON.stringify(data.data.trip.attraction.id)})
+function cleanBooking(data) {
+	fetchData(
+		'/api/booking',
+		'DELETE',
+		{
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+		JSON.stringify(data.data.trip.attraction.id)
+	);
 }
 
-getOrderNumber().then(data => {    
-    renderOrderNumber(data.data.number);
-    cleanBooking(data);
-})
+getOrderNumber().then((data) => {
+	renderOrderNumber(data.data.number);
+	cleanBooking(data);
+});
