1a0c00045298243880312af16bef37e9900b3f61
Betty <qwe50929@gmail.com>
2023-10-15 23:46:24 +0800

    feat: variable

--- static/js/tapPay.js
+++ static/js/tapPay.js
@@ -1,106 +1,114 @@
+import { getBookingData, getUserData } from './booking.js';
+
 /* 設定三個 tappay field container
-*/
+ */
 
 // 利用 TPDirect.setupSDK 設定參數
-TPDirect.setupSDK(137041, 'app_YpCfF6vfCOA8fNnCH0HKnSJlfGwctHbVTsMp8FdUkVADXMX6CgOkr1V1Hsos', 'sandbox')
-
+TPDirect.setupSDK(
+	137041,
+	'app_YpCfF6vfCOA8fNnCH0HKnSJlfGwctHbVTsMp8FdUkVADXMX6CgOkr1V1Hsos',
+	'sandbox'
+);
 
 TPDirect.card.setup({
-    fields : {
-        number: {
-            // css selector
-            element: document.getElementById('card-number'),
-            placeholder: '**** **** **** ****'
-        },
-        expirationDate: {
-            // DOM object
-            element: document.getElementById('card-expiration-date'),
-            placeholder: 'MM / YY'
-        },
-        ccv: {
-            element: document.getElementById('card-ccv'),
-            placeholder: 'ccv'
-        }
-    },
-    isMaskCreditCardNumber: true,
-            maskCreditCardNumberRange: {
-                beginIndex: 6, 
-                endIndex: 11
-            }
-})
+	fields: {
+		number: {
+			// css selector
+			element: document.getElementById('card-number'),
+			placeholder: '**** **** **** ****',
+		},
+		expirationDate: {
+			// DOM object
+			element: document.getElementById('card-expiration-date'),
+			placeholder: 'MM / YY',
+		},
+		ccv: {
+			element: document.getElementById('card-ccv'),
+			placeholder: 'ccv',
+		},
+	},
+	isMaskCreditCardNumber: true,
+	maskCreditCardNumberRange: {
+		beginIndex: 6,
+		endIndex: 11,
+	},
+});
 
-
-
-
-let submitButton = document.getElementById("submitButton");
+let submitButton = document.getElementById('submitButton');
 // TPDirect.card.onUpdated 取得 TapPay Fields 狀態
 TPDirect.card.onUpdate(function (update) {
-    // --> you can call TPDirect.card.getPrime()
-    if (update.canGetPrime) {
-        // Enable submit Button to get prime.
-        submitButton.removeAttribute('disabled')
-    } else {
-        // Disable submit Button to get prime.
-        submitButton.setAttribute('disabled', true)
-    }
-})
-
+	// --> you can call TPDirect.card.getPrime()
+	if (update.canGetPrime) {
+		// Enable submit Button to get prime.
+		submitButton.removeAttribute('disabled');
+	} else {
+		// Disable submit Button to get prime.
+		submitButton.setAttribute('disabled', true);
+	}
+});
 
 // 利用 TPDirect.card.getPrime 來取得 prime 字串
 
 // call TPDirect.card.getPrime when user submit form to get tappay prime
 // $('form').on('submit', onSubmit)
-submitButton.addEventListener("click", onSubmit);
-
-
+submitButton.addEventListener('click', onSubmit);
+const token = localStorage.getItem('token');
 function onSubmit(event) {
-    event.preventDefault()
+	event.preventDefault();
 
-    // 取得 TapPay Fields 的 status
-    const tappayStatus = TPDirect.card.getTappayFieldsStatus()
-    console.log(tappayStatus)
+	// 取得 TapPay Fields 的 status
+	const tappayStatus = TPDirect.card.getTappayFieldsStatus();
+	// console.log(tappayStatus);
 
-    // 確認是否可以 getPrime
-    if (tappayStatus.canGetPrime === false) {
-        alert('can not get prime')
-        return
-    }
-    
+	// 確認是否可以 getPrime
+	if (tappayStatus.canGetPrime === false) {
+		alert('can not get prime');
+		return;
+	}
 
-    // Get prime
-    TPDirect.card.getPrime(async(result) => {
-        // if (result.status !== 0) {
-        //     alert('get prime error ' + result.msg)
-        //     return
-        // }
-        // alert('get prime 成功，prime: ' + result.card.prime);
+	// Get prime
+	TPDirect.card.getPrime(async (result) => {
+		// if (result.status !== 0) {
+		//     alert('get prime error ' + result.msg)
+		//     return
+		// }
+		// alert('get prime 成功，prime: ' + result.card.prime);
 
-        order = await getBookingData();
-        contact = await getUserData();
-        primeData = {
-            prime: result.card.prime,
-            order: {"price":order.data.price,
-            "trip": {"attraction": order.data.attraction},
-            "date": order.data.time,
-            "time": order.data.date},
-            contact: {"name":contact.data.name, "email": contact.data.email, "phone":inputPhone.value}
-        }
+		let order = await getBookingData();
+		let contact = await getUserData();
+		let primeData = {
+			prime: result.card.prime,
+			order: {
+				price: order.data.price,
+				trip: { attraction: order.data.attraction },
+				date: order.data.time,
+				time: order.data.date,
+			},
+			contact: {
+				name: contact.data.name,
+				email: contact.data.email,
+				phone: inputPhone.value,
+			},
+		};
 
-        // send prime to your server, to pay with Pay by Prime API .
-        fetch(`/api/orders`, {method: "POST", headers: {
-            "Content-Type": "application/json", "Authorization": `Bearer ${token}`
-        }, body: JSON.stringify(primeData)})
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            window.location.href = `/thankyou?number=${data.data.number}`
-        })
-        
-        // Pay By Prime Docs: https://docs.tappaysdk.com/tutorial/zh/back.html#pay-by-prime-api
-    })
+		// send prime to your server, to pay with Pay by Prime API .
+		fetch(`/api/orders`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(primeData),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				window.location.href = `/thankyou?number=${data.data.number}`;
+			});
+
+		// Pay By Prime Docs: https://docs.tappaysdk.com/tutorial/zh/back.html#pay-by-prime-api
+	});
 }
-
-
 
 // 使用 TPDirect.card.setup 設定外觀
 // TPDirect.card.setup({
@@ -165,4 +173,3 @@ function onSubmit(event) {
 //         endIndex: 11
 //     }
 // })
-
